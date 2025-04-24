7c359a5b177b65b5c0486513c43fed255228e3d6
Pyae Phyo Zan <pyae418283@gmail.com>
2025-04-24 08:51:22 +0700

    remove unnecessary import

--- app/Repositories/RoleRepository.php
+++ app/Repositories/RoleRepository.php
@@ -2,7 +2,6 @@
 
 namespace   App\Repositories;
 
-use App\Models\Role;
 use App\Utils\BaseCrudRepository;
 
 
--- app/Repositories/UserRepository.php
+++ app/Repositories/UserRepository.php
@@ -2,7 +2,6 @@
 
 namespace   App\Repositories;
 
-use App\Models\User;
 use App\Utils\BaseCrudRepository;
 
 
